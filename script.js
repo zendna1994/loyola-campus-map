645e4dd2d2ff8eb3dec03ad480ac217af9e63f8f
ZEN - DNA <zendna1994@gmail.com>
2026-04-07 22:16:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-// Define Loyola College bounds (approx area)
-var southWest = L.latLng(13.0605, 80.2295);
-var northEast = L.latLng(13.0675, 80.2385);
-var bounds = L.latLngBounds(southWest, northEast);
+// Define Loyola College bounds (tighter box)
+var bounds = L.latLngBounds(
+  [13.0605, 80.2295],  // South-West
+  [13.0675, 80.2385]   // North-East
+);
 
 // Initialize map
 var map = L.map('map', {
   maxBounds: bounds,
-  maxBoundsViscosity: 1.0, // Strong restriction
+  maxBoundsViscosity: 1.0, // Makes edges solid
   minZoom: 16,
-  maxZoom: 19
-}).setView([13.0643, 80.2337], 17);
+  maxZoom: 19,
+  zoomControl: true
+});
 
-// Tile layer (you can change later)
+// Force map inside bounds immediately
+map.fitBounds(bounds);
+
+// Add tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  noWrap: true, // Prevent infinite world scroll
   attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-// Fit map exactly to Loyola bounds
-map.fitBounds(bounds);
+// EXTRA FORCE: reset if user tries to escape
+map.on('drag', function () {
+  map.panInsideBounds(bounds, { animate: false });
+});
 
 // Test marker
 L.marker([13.0643, 80.2337])
