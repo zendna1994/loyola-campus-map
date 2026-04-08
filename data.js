0a7d90cb06d79ba735ed099b4bd95c51bb64ddff
ZEN - DNA <zendna1994@gmail.com>
2026-04-08 08:43:01 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,47 @@
+// ===============================
+// LOYOLA CAMPUS DATA
+// ===============================
+
+var locations = [
+
+  {
+    name: "Zoology Department",
+    type: "department",
+    lat: 13.0639,
+    lng: 80.2335,
+    description: "Advanced Zoology & Biotechnology"
+  },
+
+  {
+    name: "Bio-Nexus Hitech Lab",
+    type: "lab",
+    lat: 13.0645,
+    lng: 80.2342,
+    description: "Research Laboratory"
+  },
+
+  {
+    name: "Bertram Hall",
+    type: "hall",
+    lat: 13.0652,
+    lng: 80.2328,
+    description: "Main Auditorium"
+  },
+
+  {
+    name: "Toilet Block A",
+    type: "toilet",
+    lat: 13.0640,
+    lng: 80.2329,
+    description: "Restroom Facility"
+  },
+
+  {
+    name: "Water Point",
+    type: "water",
+    lat: 13.0643,
+    lng: 80.2338,
+    description: "Drinking Water"
+  }
+
+];
