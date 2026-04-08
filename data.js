3986247cb180aad1431968a7e3b1794fc9287030
ZEN - DNA <zendna1994@gmail.com>
2026-04-08 10:01:01 +0530

    Delete data.js

--- data.js
+++
@@ -1,9 +0,0 @@
-var locations = [
-
-{ name:"Zoology Dept", type:"department", floor:"1", lat:13.0639, lng:80.2335, description:"Biotech Dept" },
-{ name:"Bio Lab", type:"lab", floor:"2", lat:13.0645, lng:80.2342, description:"Lab" },
-{ name:"Bertram Hall", type:"hall", floor:"ground", lat:13.0652, lng:80.2328, description:"Auditorium" },
-{ name:"Toilet A", type:"toilet", floor:"ground", lat:13.0640, lng:80.2329, description:"Restroom" },
-{ name:"Water Point", type:"water", floor:"ground", lat:13.0643, lng:80.2338, description:"Drinking Water" }
-
-];
