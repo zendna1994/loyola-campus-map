a8e954693eb175b265cbb1d6e5a381930c9d6469
ZEN - DNA <zendna1994@gmail.com>
2026-04-08 11:26:26 +0530

    Update config.js

--- data/config.js
+++ data/config.js
@@ -1,1 +1,1 @@
-
+const SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTm3foZlfEb4pQVdLKceGYFm6CwIihs1swXAniKzq1efqlO6XqDToAtQTbhEqp0UuGepameKCtdBt-H/pub?gid=0&single=true&output=csv";
