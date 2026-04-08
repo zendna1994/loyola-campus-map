7f66a4808ffb180328dd711e65df0ca95e5d0474
ZEN - DNA <zendna1994@gmail.com>
2026-04-08 08:16:56 +0530

    Update map.js

--- map.js
+++ map.js
@@ -1,37 +1,17 @@
-// ===============================
-// LOYOLA CAMPUS MAP - BASE MODULE
-// ===============================
-
-// 1. CREATE MAP
 var map = L.map('map', {
-  zoomControl: true
-}).setView([13.061592,80.234699], 17);
+  zoomControl: false
+}).setView([13.061592, 80.234699], 17);
 
-// --------------------------------
-// 🔧 CHANGE THIS ONLY IF NEEDED
-// Center coordinates of campus
-// Format: [latitude, longitude]
-// --------------------------------
-
-
-// 2. ADD SATELLITE TILE (CLEAN)
 L.tileLayer(
   'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
   {
     attribution: 'Tiles © Esri',
-    maxZoom: 20
+    maxZoom: 19
   }
 ).addTo(map);
 
+L.control.zoom({
+  position: 'bottomright'
+}).addTo(map);
 
-// 3. OPTIONAL CLEAN UI SETTINGS
-map.zoomControl.setPosition('bottomright');
-
-
-// 4. GLOBAL MAP ACCESS (IMPORTANT FOR NEXT MODULES)
 window.loyolaMap = map;
-
-
-// ===============================
-// END OF MODULE 1
-// ===============================
