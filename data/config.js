545fab1626c090a84845ace6c8e2f7d135ca6d81
ZEN - DNA <zendna1994@gmail.com>
2026-04-08 12:20:54 +0530

    Update config.js

--- data/config.js
+++ data/config.js
@@ -1,1 +1,4 @@
-const SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTm3foZlfEb4pQVdLKceGYFm6CwIihs1swXAniKzq1efqlO6XqDToAtQTbhEqp0UuGepameKCtdBt-H/pub?gid=0&single=true&output=csv";
+const SHEET1 = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTm3foZlfEb4pQVdLKceGYFm6CwIihs1swXAniKzq1efqlO6XqDToAtQTbhEqp0UuGepameKCtdBt-H/pub?gid=0&single=true&output=csv";
+const SHEET2 = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTm3foZlfEb4pQVdLKceGYFm6CwIihs1swXAniKzq1efqlO6XqDToAtQTbhEqp0UuGepameKCtdBt-H/pub?gid=1264507605&single=true&output=csv";
+const SHEET3 = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTm3foZlfEb4pQVdLKceGYFm6CwIihs1swXAniKzq1efqlO6XqDToAtQTbhEqp0UuGepameKCtdBt-H/pub?gid=1995610518&single=true&output=csv";
+const SHEET4 = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTm3foZlfEb4pQVdLKceGYFm6CwIihs1swXAniKzq1efqlO6XqDToAtQTbhEqp0UuGepameKCtdBt-H/pub?gid=2009515906&single=true&output=csv";
